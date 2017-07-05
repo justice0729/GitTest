93cf7fee1e37b4358bb4ddad1f0a03c00037dd42
justice0729 <justice0729@naver.com>
2017-07-05 10:03:57 +0900

    test

---
+++ test.java
@@ -0,0 +1,51 @@
+package test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Date;
+
+class Configure {
+
+   public static final boolean debug = false;
+   static final boolean TRACE = true;
+}
+
+class Test{
+	
+	String name;
+}
+
+
+
+public class test {
+	
+
+    
+	public static void main(String args[]) {
+
+		String[] imrArr={"10","11"};
+		
+		System.out.println(String.valueOf(imrArr[1].indexOf("2") + 1)); 
+		   
+    		
+   
+    	
+    	
+
+    }
+	
+	public boolean equals(Object obj){
+		
+		
+		
+		
+		
+		
+		return false;
+		
+		
+	}
+
+}
+
